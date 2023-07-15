b2d576333bb2693d7f511dbfbb4e2c897ebc8b68
Karel <k.poblete1610@gmail.com>
2023-07-14 21:13:46 -0400

    Aplicación terminada.

---
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -0,0 +1,35 @@
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import {Map, Marker} from 'mapbox-gl';
+
+
+@Component({
+  selector: 'map-mini-map',
+  templateUrl: './mini-map.component.html',
+  styleUrls: ['./mini-map.component.css']
+})
+export class MiniMapComponent {
+
+  @ViewChild('map')
+  public divMap?: ElementRef;
+
+  @Input() lngLat?: [number, number];
+
+  ngAfterViewInit(){
+    if(!this.divMap?.nativeElement) throw "Map Div no encontrado";
+    if(!this.lngLat) throw "lngLat no puede ser nulo";
+
+    const map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+      interactive: false
+    });
+
+    new Marker().setLngLat(this.lngLat).addTo(map);
+
+  }
+
+
+
+}
